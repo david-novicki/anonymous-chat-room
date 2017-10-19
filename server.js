da3a7af34eb01398f6b7998b3d2011cf88588b1f
supercycle91 <supercycle91@gmail.com>
2017-10-18 21:01:16 -0700

    added https support for socket.io

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(https);
 var https = require('https');
 var fs = require('fs');
 var messages = [];
@@ -11,34 +10,35 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/build/index.html');
 });
 
-const filterArray = (items) => {
-	while (items.length > 10) {
-		items.shift();
-	}
-	return items;
-};
-
-io.on('connection', (socket) => {
-	console.log('a user connected');
-	socket.emit('onStart', filterArray(messages));
-	socket.on('sendMessage', (message) => {
-		console.log(message);
-		messages.push(message);
-		io.sockets.emit('receivedMessage', message);
-	})
-	socket.on('disconnect', () => {
-		console.log('user disconnected');
-	});
-});
-
-http.listen(3000, () => {
-	console.log('http listening on *:3000');
-});
+// http.listen(3000, () => {
+// 	console.log('http listening on *:3000');
+// });
 if (process.env.NODE_ENV == 'production') {
 	var https_options = {
 		key: fs.readFileSync('./www.anonymous-chatroom.com.key'),
 		cert: fs.readFileSync('./www.anonymous-chatroom.com.crt')
 	};
-	https.createServer(https_options, app).listen(3001);
-	console.log('https listening on %s:%s', 3001);
+	var server = https.createServer(https_options, app);
+	const filterArray = (items) => {
+		while (items.length > 10) {
+			items.shift();
+		}
+		return items;
+	};
+	var io = require('socket.io')(server);
+	io.on('connection', (socket) => {
+		console.log('a user connected');
+		socket.emit('onStart', filterArray(messages));
+		socket.on('sendMessage', (message) => {
+			console.log(message);
+			messages.push(message);
+			io.sockets.emit('receivedMessage', message);
+		})
+		socket.on('disconnect', () => {
+			console.log('user disconnected');
+		});
+	});
+	server.listen(3001, () => {
+		console.log('https listening on ', 3001);
+	});
 }
